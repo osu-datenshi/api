d32049b823d6aa899767cf688ae79aea47a4a552
Rei Hakurei <reimu_after_marisa@yahoo.com>
2021-02-13 18:07:26 +0700

    dengerin umaru dulu

--- app/v1/user_scores.go
+++ app/v1/user_scores.go
@@ -19,42 +19,62 @@ type userScoresResponse struct {
 	Scores []userScore `json:"scores"`
 }
 
+const masterMode = true
+const masterScoreSelectBase = `
+		SELECT
+			s.id, s.beatmap_md5, s.score,
+			s.max_combo, s.full_combo, s.mods,
+			s.300_count, s.100_count, s.50_count,
+			s.gekis_count, s.katus_count, s.misses_count,
+			s.time, s.play_mode, s.accuracy, s.pp,
+			s.completed,
+
+			b.beatmap_id, b.beatmapset_id, b.beatmap_md5,
+			b.song_name, b.ar, b.od, b.difficulty_std,
+			b.difficulty_taiko, b.difficulty_ctb, b.difficulty_mania,
+			b.max_combo, b.hit_length, b.ranked,
+			b.ranked_status_freezed, b.latest_update
+		FROM scores_master as s
+		INNER JOIN beatmaps as b ON b.beatmap_md5 = s.beatmap_md5
+		INNER JOIN users ON users.id = s.userid
+		`
+
 const relaxScoreSelectBase = `
 		SELECT
-			scores_relax.id, scores_relax.beatmap_md5, scores_relax.score,
-			scores_relax.max_combo, scores_relax.full_combo, scores_relax.mods,
-			scores_relax.300_count, scores_relax.100_count, scores_relax.50_count,
-			scores_relax.gekis_count, scores_relax.katus_count, scores_relax.misses_count,
-			scores_relax.time, scores_relax.play_mode, scores_relax.accuracy, scores_relax.pp,
-			scores_relax.completed,
+			s.id, s.beatmap_md5, s.score,
+			s.max_combo, s.full_combo, s.mods,
+			s.300_count, s.100_count, s.50_count,
+			s.gekis_count, s.katus_count, s.misses_count,
+			s.time, s.play_mode, s.accuracy, s.pp,
+			s.completed,
 
-			beatmaps.beatmap_id, beatmaps.beatmapset_id, beatmaps.beatmap_md5,
-			beatmaps.song_name, beatmaps.ar, beatmaps.od, beatmaps.difficulty_std,
-			beatmaps.difficulty_taiko, beatmaps.difficulty_ctb, beatmaps.difficulty_mania,
-			beatmaps.max_combo, beatmaps.hit_length, beatmaps.ranked,
-			beatmaps.ranked_status_freezed, beatmaps.latest_update
-		FROM scores_relax
-		INNER JOIN beatmaps ON beatmaps.beatmap_md5 = scores_relax.beatmap_md5
-		INNER JOIN users ON users.id = scores_relax.userid
+			b.beatmap_id, b.beatmapset_id, b.beatmap_md5,
+			b.song_name, b.ar, b.od, b.difficulty_std,
+			b.difficulty_taiko, b.difficulty_ctb, b.difficulty_mania,
+			b.max_combo, b.hit_length, b.ranked,
+			b.ranked_status_freezed, b.latest_update
+		FROM scores_relax as s
+		INNER JOIN beatmaps as b ON b.beatmap_md5 = s.beatmap_md5
+		INNER JOIN users ON users.id = s.userid
 		`
 
 const userScoreSelectBase = `
 		SELECT
-			scores.id, scores.beatmap_md5, scores.score,
-			scores.max_combo, scores.full_combo, scores.mods,
-			scores.300_count, scores.100_count, scores.50_count,
-			scores.gekis_count, scores.katus_count, scores.misses_count,
-			scores.time, scores.play_mode, scores.accuracy, scores.pp,
-			scores.completed,
+			s.id, s.beatmap_md5, s.score,
+			s.max_combo, s.full_combo, s.mods,
+			s.300_count, s.100_count, s.50_count,
+			s.gekis_count, s.katus_count, s.misses_count,
+			s.time, s.play_mode, s.accuracy, s.pp,
+			s.completed,
 
-			beatmaps.beatmap_id, beatmaps.beatmapset_id, beatmaps.beatmap_md5,
-			beatmaps.song_name, beatmaps.ar, beatmaps.od, beatmaps.difficulty_std,
-			beatmaps.difficulty_taiko, beatmaps.difficulty_ctb, beatmaps.difficulty_mania,
-			beatmaps.max_combo, beatmaps.hit_length, beatmaps.ranked,
-			beatmaps.ranked_status_freezed, beatmaps.latest_update
-		FROM scores
-		INNER JOIN beatmaps ON beatmaps.beatmap_md5 = scores.beatmap_md5
-		INNER JOIN users ON users.id = scores.userid
+			b.beatmap_id, b.beatmapset_id, b.beatmap_md5,
+			b.song_name, b.ar, b.od, b.difficulty_std,
+			b.difficulty_taiko, b.difficulty_ctb, b.difficulty_mania,
+			b.max_combo, b.hit_length, b.ranked,
+			b.ranked_status_freezed, b.latest_update
+		FROM scores as s
+		INNER JOIN beatmaps ON b.beatmap_md5 = s.beatmap_md5
+		INNER JOIN users ON users.id = s.userid
 		`
 
 // UserScoresBestGET retrieves the best scores of an user, sorted by PP if
@@ -66,29 +86,51 @@ func UserScoresBestGET(md common.MethodData) common.CodeMessager {
 	}
 	
 	mc := genModeClause(md)
+	smode := common.Int(md.Query("smode"))
+	if smode < 0 || smode > 2 {
+		// enforce to vanilla
+		smode = 0
+	}
+	if !md.HasQuery("smode") && common.Int(md.Query("rx")) {
+		// retain old behavior
+		smode = 1
+	}
 	// For all modes that have PP, we leave out 0 PP scores.
 
-	if common.Int(md.Query("rx")) != 0 {
-		mc = strings.Replace(mc, "scores.", "scores_relax.", 1)
-		return relaxPuts(md, fmt.Sprintf(
+	if masterMode {
+		return masterPuts(md, fmt.Sprintf(
 			`WHERE
-				scores_relax.completed = '3'
+				s.completed = '3'
 				AND %s
 				%s
-				AND `+md.User.OnlyUserPublic(true)+`
-			ORDER BY scores_relax.pp DESC, scores_relax.score DESC %s`,
-			wc, mc, common.Paginate(md.Query("p"), md.Query("l"), 100),
+				AND s.special_mode = %d
+				AND %s
+			ORDER BY s.pp DESC, s.score DESC %s`,
+			wc, mc, smode, md.User.OnlyUserPublic(true), common.Paginate(md.Query("p"), md.Query("l"), 100),
 		), param)
 	} else {
-		return scoresPuts(md, fmt.Sprintf(
-			`WHERE
-				scores.completed = '3'
-				AND %s
-				%s
-				AND `+md.User.OnlyUserPublic(true)+`
-			ORDER BY scores.pp DESC, scores.score DESC %s`,
-			wc, mc, common.Paginate(md.Query("p"), md.Query("l"), 100),
-		), param)
+		switch smode {
+			case 1:
+				return relaxPuts(md, fmt.Sprintf(
+					`WHERE
+						s.completed = '3'
+						AND %s
+						%s
+						AND %s
+					ORDER BY s.pp DESC, s.score DESC %s`,
+					wc, mc, md.User.OnlyUserPublic(true), common.Paginate(md.Query("p"), md.Query("l"), 100),
+				), param)
+			default:
+				return scoresPuts(md, fmt.Sprintf(
+					`WHERE
+						s.completed = '3'
+						AND %s
+						%s
+						AND %s
+					ORDER BY s.pp DESC, s.score DESC %s`,
+					wc, mc, md.User.OnlyUserPublic(true), common.Paginate(md.Query("p"), md.Query("l"), 100),
+				), param)
+		}
 	}
 }
 
@@ -99,26 +141,101 @@ func UserScoresRecentGET(md common.MethodData) common.CodeMessager {
 		return *cm
 	}
 	mc := genModeClause(md)
-	if common.Int(md.Query("rx")) != 0 {
-		mc = strings.Replace(mc, "scores.", "scores_relax.", 1)
-		return relaxPuts(md, fmt.Sprintf(
+	smode := common.Int(md.Query("smode"))
+	if smode < 0 || smode > 2 {
+		// enforce to vanilla
+		smode = 0
+	}
+	if !md.HasQuery("smode") && common.Int(md.Query("rx")) {
+		// retain old behavior
+		smode = 1
+	}
+	if masterMode {
+		return masterPuts(md, fmt.Sprintf(
 			`WHERE
 				%s
 				%s
-				AND `+md.User.OnlyUserPublic(true)+`
-			ORDER BY scores_relax.id DESC %s`,
-			wc, mc, common.Paginate(md.Query("p"), md.Query("l"), 100),
+				AND s.special_mode = %d
+				AND %s
+			ORDER BY s.id DESC %s`,
+			wc, mc, smode, md.User.OnlyUserPublic(true), common.Paginate(md.Query("p"), md.Query("l"), 100),
 		), param)
 	} else {
-		return scoresPuts(md, fmt.Sprintf(
-			`WHERE
-				%s
-				%s
-				AND `+md.User.OnlyUserPublic(true)+`
-			ORDER BY scores.id DESC %s`,
-			wc, genModeClause(md), common.Paginate(md.Query("p"), md.Query("l"), 100),
-		), param)
+		switch smode {
+			case 1:
+				return relaxPuts(md, fmt.Sprintf(
+					`WHERE
+						%s
+						%s
+						AND `+md.User.OnlyUserPublic(true)+`
+					ORDER BY s.id DESC %s`,
+					wc, mc, common.Paginate(md.Query("p"), md.Query("l"), 100),
+				), param)
+		  default:
+				return scoresPuts(md, fmt.Sprintf(
+					`WHERE
+						%s
+						%s
+						AND `+md.User.OnlyUserPublic(true)+`
+					ORDER BY s.id DESC %s`,
+					wc, mc, common.Paginate(md.Query("p"), md.Query("l"), 100),
+				), param)
+		}
 	}
+}
+
+func genericPuts(rows interface{}, md common.MethodData) common.CodeMessager {
+	err := nil
+	var scores []userScore
+	for rows.Next() {
+		var (
+			us userScore
+			b  beatmap
+		)
+		err = rows.Scan(
+			&us.ID, &us.BeatmapMD5, &us.Score.Score,
+			&us.MaxCombo, &us.FullCombo, &us.Mods,
+			&us.Count300, &us.Count100, &us.Count50,
+			&us.CountGeki, &us.CountKatu, &us.CountMiss,
+			&us.Time, &us.PlayMode, &us.Accuracy, &us.PP,
+			&us.Completed,
+
+			&b.BeatmapID, &b.BeatmapsetID, &b.BeatmapMD5,
+			&b.SongName, &b.AR, &b.OD, &b.Diff2.STD,
+			&b.Diff2.Taiko, &b.Diff2.CTB, &b.Diff2.Mania,
+			&b.MaxCombo, &b.HitLength, &b.Ranked,
+			&b.RankedStatusFrozen, &b.LatestUpdate,
+		)
+		if err != nil {
+			md.Err(err)
+			return Err500
+		}
+		b.Difficulty = b.Diff2.STD
+		us.Beatmap = b
+		us.Rank = strings.ToUpper(getrank.GetRank(
+			osuapi.Mode(us.PlayMode),
+			osuapi.Mods(us.Mods),
+			us.Accuracy,
+			us.Count300,
+			us.Count100,
+			us.Count50,
+			us.CountMiss,
+		))
+		scores = append(scores, us)
+	}
+	r := userScoresResponse{}
+	r.Code = 200
+	r.Scores = scores
+	return r
+}
+
+func masterPuts(md common.MethodData, whereClause string, params ...interface{}) common.CodeMessager {
+	rows, err := md.DB.Query(masterScoreSelectBase+whereClause, params...)
+	if err != nil {
+		md.Err(err)
+		return Err500
+	}
+	return genericPuts(rows, md)
 }
 
 func scoresPuts(md common.MethodData, whereClause string, params ...interface{}) common.CodeMessager {
@@ -127,47 +244,7 @@ func scoresPuts(md common.MethodData, whereClause string, params ...interface{})
 		md.Err(err)
 		return Err500
 	}
-	var scores []userScore
-	for rows.Next() {
-		var (
-			us userScore
-			b  beatmap
-		)
-		err = rows.Scan(
-			&us.ID, &us.BeatmapMD5, &us.Score.Score,
-			&us.MaxCombo, &us.FullCombo, &us.Mods,
-			&us.Count300, &us.Count100, &us.Count50,
-			&us.CountGeki, &us.CountKatu, &us.CountMiss,
-			&us.Time, &us.PlayMode, &us.Accuracy, &us.PP,
-			&us.Completed,
-
-			&b.BeatmapID, &b.BeatmapsetID, &b.BeatmapMD5,
-			&b.SongName, &b.AR, &b.OD, &b.Diff2.STD,
-			&b.Diff2.Taiko, &b.Diff2.CTB, &b.Diff2.Mania,
-			&b.MaxCombo, &b.HitLength, &b.Ranked,
-			&b.RankedStatusFrozen, &b.LatestUpdate,
-		)
-		if err != nil {
-			md.Err(err)
-			return Err500
-		}
-		b.Difficulty = b.Diff2.STD
-		us.Beatmap = b
-		us.Rank = strings.ToUpper(getrank.GetRank(
-			osuapi.Mode(us.PlayMode),
-			osuapi.Mods(us.Mods),
-			us.Accuracy,
-			us.Count300,
-			us.Count100,
-			us.Count50,
-			us.CountMiss,
-		))
-		scores = append(scores, us)
-	}
-	r := userScoresResponse{}
-	r.Code = 200
-	r.Scores = scores
-	return r
+	return genericPuts(rows, md)
 }
 
 func relaxPuts(md common.MethodData, whereClause string, params ...interface{}) common.CodeMessager {
@@ -176,45 +253,5 @@ func relaxPuts(md common.MethodData, whereClause string, params ...interface{}) 
 		md.Err(err)
 		return Err500
 	}
-	var scores []userScore
-	for rows.Next() {
-		var (
-			us userScore
-			b  beatmap
-		)
-		err = rows.Scan(
-			&us.ID, &us.BeatmapMD5, &us.Score.Score,
-			&us.MaxCombo, &us.FullCombo, &us.Mods,
-			&us.Count300, &us.Count100, &us.Count50,
-			&us.CountGeki, &us.CountKatu, &us.CountMiss,
-			&us.Time, &us.PlayMode, &us.Accuracy, &us.PP,
-			&us.Completed,
-
-			&b.BeatmapID, &b.BeatmapsetID, &b.BeatmapMD5,
-			&b.SongName, &b.AR, &b.OD, &b.Diff2.STD,
-			&b.Diff2.Taiko, &b.Diff2.CTB, &b.Diff2.Mania,
-			&b.MaxCombo, &b.HitLength, &b.Ranked,
-			&b.RankedStatusFrozen, &b.LatestUpdate,
-		)
-		if err != nil {
-			md.Err(err)
-			return Err500
-		}
-		b.Difficulty = b.Diff2.STD
-		us.Beatmap = b
-		us.Rank = strings.ToUpper(getrank.GetRank(
-			osuapi.Mode(us.PlayMode),
-			osuapi.Mods(us.Mods),
-			us.Accuracy,
-			us.Count300,
-			us.Count100,
-			us.Count50,
-			us.CountMiss,
-		))
-		scores = append(scores, us)
-	}
-	r := userScoresResponse{}
-	r.Code = 200
-	r.Scores = scores
-	return r
+	return genericPuts(rows, md)
 }
